9ac7f2dc7e787238fc7172e35556665212f27baf
partdark <fhfhfhfhfhfhfhfhffhh@mail.ru>
2023-02-12 20:17:20 +0500

    new version

--- app/src/main/java/com/example/pr6/MainActivity1.kt
+++ app/src/main/java/com/example/pr6/MainActivity1.kt
@@ -1,15 +1,75 @@
 package com.example.pr6
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+
 
 class MainActivity1 : AppCompatActivity() {
+    var colors = arrayListOf(Color.GREEN,Color.WHITE,Color.RED, Color.CYAN)
+    var sheetNumber = 0
+    var textSize = 18F
+
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity1)
+              val text : EditText = findViewById(R.id.text)
+        sheetNumber = getIntent().getIntExtra("sheetNumber",0)
         val buttonNext : Button = findViewById(R.id.next1)
-        buttonNext.setOnClickListener {  val intent = Intent (this, MainActivity2::class.java) }
+                buttonNext.setOnClickListener {
+            if (sheetNumber < colors.size -1)
+            {
+                val intent = Intent (this, this::class.java)
+                intent.putExtra("sheetNumber", sheetNumber + 1)
+                startActivity(intent)
+            } else {
+                Toast.makeText(applicationContext, "its all",Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
+
+
+
+    override fun onPause() {
+        val text : EditText = findViewById(R.id.text)
+        super.onPause()
+        val pref = getPreferences(MODE_PRIVATE).edit()
+
+        pref.putString("note" + sheetNumber, text.editableText.toString())
+        pref.apply()
+    }
+
+    override fun onResume() {
+        val text : EditText = findViewById(R.id.text)
+        super.onResume()
+        val sheet : ConstraintLayout = findViewById(R.id.sheet1)
+       sheet.setBackgroundColor(colors[sheetNumber])
+        text.textSize =textSize
+        val saveState = getPreferences(Context.MODE_PRIVATE).getString("note" + sheetNumber, null)
+        if (saveState !=null) {
+            text.setText(saveState)
+        }
+    }
+
+ fun newSize (view : View) {
+
+val size : EditText = findViewById(R.id.new_size)
+         val text : EditText = findViewById(R.id.text)
+     textSize = (size.text.toString()).toFloat()
+     text.textSize = textSize
+ }
+
+
+
+
 }
